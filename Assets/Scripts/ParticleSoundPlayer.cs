d7b7d206960a65ac229798b50add1601497beb22
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-05 22:30:27 -0700

    Added audio and SFX

---
+++ Assets/Scripts/ParticleSoundPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(AudioSource))]
+public class ParticleSoundPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip bornSound;
+    [SerializeField] private AudioClip deathSound;
+
+    private ParticleSystem _particle;
+    private AudioSource _audioSource;
+    private int _currentNumberOfParticles;
+
+    void Start()
+    {
+        _particle = GetComponent<ParticleSystem>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        var particleCount = _particle.particleCount;
+        if (particleCount < _currentNumberOfParticles)
+        {
+            if (deathSound)
+            {
+                _audioSource.PlayOneShot(deathSound);
+            }
+        }
+        if (particleCount > _currentNumberOfParticles)
+        {
+            if (bornSound)
+            {
+                _audioSource.PlayOneShot(bornSound);
+            }
+        }
+
+        _currentNumberOfParticles = particleCount;
+    }
+}
