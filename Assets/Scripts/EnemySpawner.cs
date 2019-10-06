d31a9a0eea330af1b9466c5b70d0b3215be160d3
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-05 21:34:07 -0700

    Added an enemy spawner and a self destruct routine for the particle FX (temporary)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyToSpawn;
+    [SerializeField] private float spawnTime = 3f;
+    [SerializeField] private int spawnCount = 10;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (spawnCount > 0)
+        {
+            var enemy = Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+            enemy.transform.parent = transform.parent;
+            spawnCount--;
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
