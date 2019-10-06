89801a5d880ba7da274d77c704aca1e75d909289
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-05 20:16:53 -0700

    Refactored the enemy damage script for cleanliness

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private int enemyHitPoints = 15;
+    [SerializeField] private GameObject enemyExplosionFx;
+    [SerializeField] private GameObject enemyDamageFx;
+    private int _damage = 1;
+    
+    private void OnParticleCollision(GameObject otherCollider)
+    {
+        DamageEnemy(_damage, otherCollider.transform);  //todo: get damage from colliding object
+    }
+
+    public void DamageEnemy(int damage, Transform damageTransform)
+    {
+        enemyHitPoints -= damage;
+        if (enemyHitPoints <= 0)
+        {
+            // Kill the enemy if the HP goes below zero
+            DestroyEnemy();
+        }
+        else
+        {
+            // Trigger damage effect
+            Instantiate(enemyDamageFx, transform.position, Quaternion.Inverse(damageTransform.rotation));
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Instantiate(enemyExplosionFx, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,10 +7,6 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path;
     [SerializeField] private float dwellTime = 1f;
-    [SerializeField] private int enemyHitPoints = 15;
-    [SerializeField] private GameObject enemyExplosionFx;
-    [SerializeField] private GameObject enemyDamageFx;
-    private int _damage = 1;
 
     public void SetPath(List<Waypoint> newPath)
     {
@@ -25,31 +21,5 @@ public class EnemyMovement : MonoBehaviour
             transform.position = waypoint.transform.position;
             yield return new WaitForSeconds(dwellTime);
         }
-    }
-
-    private void OnParticleCollision(GameObject otherCollider)
-    {
-        DamageEnemy(_damage, otherCollider.transform);  //todo: get damage from colliding object
-    }
-
-    public void DamageEnemy(int damage, Transform damageTransform)
-    {
-        enemyHitPoints -= damage;
-        if (enemyHitPoints <= 0)
-        {
-            // Kill the enemy if the HP goes below zero
-            DestroyEnemy();
-        }
-        else
-        {
-            // Trigger damage effect
-            Instantiate(enemyDamageFx, transform.position, Quaternion.Inverse(damageTransform.rotation));
-        }
-    }
-
-    private void DestroyEnemy()
-    {
-        Instantiate(enemyExplosionFx, transform.position, Quaternion.identity);
-        Destroy(gameObject);
     }
 }
