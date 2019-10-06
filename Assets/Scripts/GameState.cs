78a48d2e6c524c03d0d5631fbe4c00624d431d1b
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-05 17:57:27 -0700

    Added a tower that follows the enemy movement

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    private bool _shouldReload;
+
+    private void Update()
+    {
+        GetInput();
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (_shouldReload)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetButton("Fire2") && !_shouldReload)
+        {
+            _shouldReload = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private Transform towerHead;
+    [SerializeField] private Transform enemyTarget;
+    
+    void Update()
+    {
+        LookAtEnemy();
+    }
+
+    private void LookAtEnemy()
+    {
+        towerHead.LookAt(enemyTarget);
+    }
+}
