84ed3b5ded781d4be02299d7eb9658bc4b057cb4
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-06 20:27:31 -0700

    Added a tower limit and a ring buffer to place and move them

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Instantiator : MonoBehaviour
 {
     [SerializeField] private Tower selectedTower;
-    private readonly List<Waypoint> _occupiedBlocks = new List<Waypoint>();
-    
+    [SerializeField] private int maxNoOfTowers = 5;
+    private Queue<Waypoint> _occupiedBlocks;
+    private Queue<Tower> _towers;
+
+    private void Start()
+    {
+        _occupiedBlocks = new Queue<Waypoint>(maxNoOfTowers);
+        _towers = new Queue<Tower>(maxNoOfTowers);
+    }
+
     public void PlaceSelectedAt(Waypoint waypoint)
     {
-        if (!_occupiedBlocks.Contains(waypoint))
+        if (_occupiedBlocks.Contains(waypoint)) return;
+        
+        Tower tower;
+        if (_towers.Count < maxNoOfTowers)
         {
-            Instantiate(selectedTower, waypoint.transform.position, Quaternion.identity, transform);
-            _occupiedBlocks.Add(waypoint);
+            tower = Instantiate(selectedTower);
         }
+        else
+        {
+            tower = _towers.Dequeue();
+            _occupiedBlocks.Dequeue();
+        }
+        tower.transform.SetPositionAndRotation(waypoint.transform.position, Quaternion.identity);
+        _towers.Enqueue(tower);
+        _occupiedBlocks.Enqueue(waypoint);
     }
 }
